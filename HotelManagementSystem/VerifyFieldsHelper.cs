dfab1cff7d85b7f8c94e01f268218234f7a06c8d
RainbowInHell <matveyrfh@mail.ru>
2022-11-25 23:09:27 +0300

    VerifyFieldsHelper class has been added with first method

---
+++ HotelManagementSystem/VerifyFieldsHelper.cs
@@ -0,0 +1,11 @@
+﻿
+namespace HotelManagementSystem
+{
+    public static class VerifyFieldsHelper
+    {
+        public static bool IsAnyOfFieldsNullOrEmpty(params string[] values)
+        {
+            return values.Any(x => string.IsNullOrEmpty(x)) ? true : false;
+        }
+    }
+}

f5687682370a5032c81d0edf574221d492009a6f
RainbowInHell <matveyrfh@mail.ru>
2022-11-26 15:08:52 +0300

    Hotel's model has been added to the project

---
+++ HotelManagementSystem/Models/Hotel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotelManagementSystem.Models;
+
+public partial class Hotel
+{
+    public long HotelId { get; set; }
+
+    public string HotelName { get; set; } = null!;
+
+    public string HotelContactNumber { get; set; } = null!;
+
+    public string HotelEmail { get; set; } = null!;
+
+    public string HotelWebsite { get; set; } = null!;
+
+    public string HotelDescription { get; set; } = null!;
+
+    public long FloorsCount { get; set; }
+
+    public string HotelCity { get; set; } = null!;
+
+    public long TotalRooms { get; set; }
+
+    public string HotelCountry { get; set; } = null!;
+
+    public string HotelAddress { get; set; } = null!;
+
+    public string HotelZip { get; set; } = null!;
+}

d9db7c10ab94053ac50e97f10093ad4fdfd68f57
RainbowInHell <matveyrfh@mail.ru>
2022-11-26 15:11:30 +0300

    HotelDbContext update
    New model logic has been added

--- HotelManagementSystem/Models/HotelDbContext.cs
+++ HotelManagementSystem/Models/HotelDbContext.cs
@@ -17,12 +17,27 @@ public partial class HotelDbContext : DbContext
 
     public virtual DbSet<Admin> Admins { get; set; }
 
+    public virtual DbSet<Hotel> Hotels { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlite("Data Source=D:\\College_Of_Business_and_Law(2019-2023)\\BD_AND_SUBD\\CourseWork\\Hotel_Management_System\\HotelManagementSystem\\DataBaseSourceFile\\HotelDB.db");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Hotel>(entity =>
+        {
+            entity.HasIndex(e => e.HotelAddress, "IX_Hotels_HotelAddress").IsUnique();
+
+            entity.HasIndex(e => e.HotelContactNumber, "IX_Hotels_HotelContactNumber").IsUnique();
+
+            entity.HasIndex(e => e.HotelEmail, "IX_Hotels_HotelEmail").IsUnique();
+
+            entity.HasIndex(e => e.HotelName, "IX_Hotels_HotelName").IsUnique();
+
+            entity.HasIndex(e => e.HotelWebsite, "IX_Hotels_HotelWebsite").IsUnique();
+        });
+
         OnModelCreatingPartial(modelBuilder);
     }
 

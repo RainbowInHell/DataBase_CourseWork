62cb7e1ff4398d93a96d86d21ec336f3d07ad20a
RainbowInHell <matveyrfh@mail.ru>
2022-12-01 23:08:20 +0300

    New entity has been added

---
+++ HotelManagementSystem/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotelManagementSystem.Models
+{
+    public partial class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeFirstName { get; set; } = null!;
+        public string EmployeeLastName { get; set; } = null!;
+        public string EmployeeLogin { get; set; } = null!;
+        public string EmployeePassword { get; set; } = null!;
+        public string EmployeeEmailAddress { get; set; } = null!;
+        public string EmployeeContactNumber { get; set; } = null!;
+        public DateOnly EmployeeBirthDate { get; set; }
+        public string EmployeeAdress { get; set; } = null!;
+        public int? DepartmentId { get; set; }
+        public int HotelId { get; set; }
+
+        public virtual Department? Department { get; set; }
+        public virtual Hotel Hotel { get; set; } = null!;
+    }
+}
